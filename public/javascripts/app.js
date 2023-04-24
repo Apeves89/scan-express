51aa20a7411dc511f2641434171deae34cf28c5c
Alexis Peves <alexpeves@gmail.com>
2023-04-24 10:57:51 -0400

    moved javascript

---
+++ public/javascripts/app.js
@@ -0,0 +1,18 @@
+import { Html5QrcodeScanner } from "https://unpkg.com/html5-qrcode";
+const scanner = new Html5QrcodeScanner('reader',{
+    qrbox:{
+      width:500,
+      height:250,
+    },fps:20,
+  });
+
+  scanner.render(success,error);
+
+  function success(result) {
+    document.getElementById('result').innerHTML = `<h2>${result}</h2>`;
+    console.log(result);
+    scanner.clear()
+  }
+  function error(err){
+    console.log(err);
+  }
