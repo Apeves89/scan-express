0c973d5c6b207e2f60b9b8a9959e73cc5ed754f6
Alexis Peves <alexpeves@gmail.com>
2023-04-24 15:53:47 -0400

    url

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 router.get('/search', function(req, res, next) {
   const upc = req.query.upc
   const options = {
-    url:process.env.API_START+upc+process.env.API_END
+    url:`${process.env.API_START}${upc}${process.env.API_END}`
   }
   request(options,function(err,response,body) {
     const data = JSON.parse(body)
